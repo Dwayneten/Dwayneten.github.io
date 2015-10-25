1df6a80ecdd24100b3411688417ec997f5d3fbb8
DwayneTen <151809668@qq.com>
2015-10-25 13:01:49 +0800

    Update functions and  Add data

--- eryazuoye.js
+++ eryazuoye.js
@@ -10,8 +10,12 @@ var frDoc = document.getElementById('_fr').contentDocument;
 var imgList = frDoc.getElementsByTagName('img');
 var ddList = frDoc.getElementsByTagName('dd');
 var judAns = 0;
+var judScan = 0;
 function getQuestionList() {
     questionList = [];
+    frDoc = document.getElementById('_fr').contentDocument;
+    imgList = frDoc.getElementsByTagName('img');
+    ddList = frDoc.getElementsByTagName('dd');
     // fetch from unsubmit homework
     if (judAns == 1) {
         ddList = [];
@@ -32,6 +36,22 @@ function getQuestionList() {
                 } else {
                     questionList.push(new Question(imgList[i].src.split('=')[1], 2));
                 }
+            } else {
+                alert('ddList[' + i + '].children[0].nodeName do not match.')
+                break;
+            }
+        }
+        return;
+    }
+    // fetch from empty homework
+    if (judScan == 1) {
+        for (i=0; i<ddList.length; i++) {
+            ddList[i].className += "noansdd";
+            imgList[i].className += "noansimg";
+            if (ddList[i].children[0].nodeName == "UL") {
+                questionList.push(new Question(imgList[i].src.split('=')[1], 5));
+            } else if (ddList[i].children[0].nodeName == "LABEL") {
+                questionList.push(new Question(imgList[i].src.split('=')[1], 5));
             } else {
                 alert('ddList[' + i + '].children[0].nodeName do not match.')
                 break;
@@ -101,21 +121,44 @@ function loadStyleString(css) {
     var head = document.getElementsByTagName("head")[0];
     head.appendChild(style);
 }
-var cssLayout = '.mybox{width: 240px;min-height: 100px;border: 1px solid #ccc;box-shadow: 0 0 8px 1px #999;position: fixed;z-index: 10;margin: 0;left: 10px;background-color: #fcfcfc;font-family: "Microsoft Jhenghei","Hiragino Sans GB","Helvetica Neue","Helvetica","WenQuanYi Micro Hei","Microsoft YaHei",Arial,sans-serif;font-size: 1.2em;}#tipbox{bottom: 10px}#jsonbox{top: 10px}.btn{margin: 20px;background-color: #289fff;text-decoration: none;display: inline-block;width: 80px;line-height: 30px;text-align: center;color: #fff;}.btn:hover{text-decoration: none;color: #fff;text-shadow: #86ffff 0 0 5px;line-height: 30px;}.centerText{text-align: center;}';
+var cssLayout = '.mybox{width: 240px;min-height: 70px;border: 1px solid #ccc;box-shadow: 0 0 8px 1px #999;position: fixed;z-index: 10;margin: 0;left: 10px;background-color: #fcfcfc;font-family: "Microsoft Jhenghei","Hiragino Sans GB","Helvetica Neue","Helvetica","WenQuanYi Micro Hei","Microsoft YaHei",Arial,sans-serif;font-size: 1.2em;}#tipbox{bottom: 10px;}#jsonbox{top: 10px;}#devbox{bottom: 120px;}.btn{margin: 20px 20px 10px 20px;background-color: #289fff;text-decoration: none;display: inline-block;width: 80px;line-height: 30px;text-align: center;color: #fff;}.btn:hover{text-decoration: none;color: #fff;text-shadow: #86ffff 0 0 5px;line-height: 30px;}.centerText{text-align: center;}';
 var xinli1Json = '[{"id":"192547","ans":2},{"id":"192559","ans":3},{"id":"192551","ans":4},{"id":"192545","ans":4},{"id":"192575","ans":3},{"id":"192543","ans":3},{"id":"192573","ans":2},{"id":"192553","ans":1},{"id":"192541","ans":1},{"id":"192549","ans":2},{"id":"192539","ans":1},{"id":"192557","ans":1},{"id":"192531","ans":1},{"id":"192535","ans":1},{"id":"192561","ans":1},{"id":"192565","ans":1},{"id":"192577","ans":1},{"id":"192563","ans":1},{"id":"192555","ans":1},{"id":"192569","ans":1},{"id":"192571","ans":3},{"id":"192567","ans":2},{"id":"192533","ans":1},{"id":"192537","ans":1}]';
 var xinli2Json = '[{"id":"192669","ans":4},{"id":"192667","ans":3},{"id":"192665","ans":3},{"id":"192701","ans":2},{"id":"192663","ans":2},{"id":"192673","ans":1},{"id":"192689","ans":1},{"id":"192697","ans":1},{"id":"192675","ans":2},{"id":"192687","ans":1},{"id":"192683","ans":1},{"id":"192671","ans":2},{"id":"192679","ans":1},{"id":"192699","ans":1},{"id":"192693","ans":2},{"id":"192677","ans":1},{"id":"192685","ans":2},{"id":"192691","ans":2},{"id":"192681","ans":1},{"id":"192695","ans":1}]';
 var xinli3Json = '[{"id":"192825","ans":2},{"id":"192823","ans":1},{"id":"192819","ans":2},{"id":"192821","ans":1},{"id":"192829","ans":1},{"id":"192827","ans":2},{"id":"192831","ans":1},{"id":"192817","ans":1},{"id":"192815","ans":1}]';
 var luoji1Json = '[{"id":"147739","ans":4},{"id":"147725","ans":1},{"id":"147729","ans":1},{"id":"147709","ans":4},{"id":"147731","ans":3},{"id":"147713","ans":1},{"id":"147693","ans":2},{"id":"147697","ans":3},{"id":"147691","ans":4},{"id":"147751","ans":2},{"id":"147705","ans":2},{"id":"147703","ans":1},{"id":"147745","ans":1},{"id":"147735","ans":2},{"id":"147717","ans":1},{"id":"147719","ans":2},{"id":"147757","ans":1},{"id":"147759","ans":2},{"id":"147747","ans":2},{"id":"147733","ans":1}]';
+var chuangye1Json = '[{"id":"83224","ans":3},{"id":"83214","ans":3},{"id":"83219","ans":3},{"id":"83218","ans":1},{"id":"52257","ans":2},{"id":"52251","ans":3},{"id":"83235","ans":1},{"id":"83238","ans":2},{"id":"52256","ans":2},{"id":"83213","ans":4},{"id":"83223","ans":4},{"id":"83237","ans":4},{"id":"83226","ans":4},{"id":"83216","ans":1},{"id":"83240","ans":2},{"id":"83222","ans":2},{"id":"83227","ans":2},{"id":"83233","ans":2},{"id":"83239","ans":1},{"id":"83221","ans":1},{"id":"83228","ans":1},{"id":"83232","ans":2},{"id":"83234","ans":1},{"id":"83220","ans":3},{"id":"83236","ans":3},{"id":"83225","ans":2},{"id":"83231","ans":3}]';
+var chuangye2Json = '[{"id":"83290","ans":2},{"id":"83284","ans":3},{"id":"83278","ans":1},{"id":"83279","ans":3},{"id":"83283","ans":1},{"id":"83297","ans":4},{"id":"83280","ans":2},{"id":"83291","ans":4},{"id":"52311","ans":4},{"id":"83286","ans":1},{"id":"83298","ans":1},{"id":"83287","ans":2},{"id":"83302","ans":2},{"id":"83303","ans":1},{"id":"83294","ans":1},{"id":"83301","ans":1},{"id":"83293","ans":1},{"id":"83289","ans":2},{"id":"83292","ans":2},{"id":"83282","ans":2},{"id":"52305","ans":4},{"id":"83299","ans":1},{"id":"83285","ans":2},{"id":"83288","ans":1}]';
+var chuangye3Json = '[{"id":"83378","ans":3},{"id":"83377","ans":2},{"id":"83371","ans":4},{"id":"83375","ans":1},{"id":"83382","ans":2},{"id":"83386","ans":4},{"id":"83388","ans":3},{"id":"83368","ans":4},{"id":"83376","ans":3},{"id":"83383","ans":1},{"id":"83381","ans":1},{"id":"83374","ans":2},{"id":"83390","ans":2},{"id":"83370","ans":1},{"id":"83389","ans":2},{"id":"83369","ans":2},{"id":"83391","ans":1},{"id":"83380","ans":1},{"id":"83379","ans":2},{"id":"83372","ans":2},{"id":"83366","ans":3},{"id":"83367","ans":4},{"id":"83384","ans":4}]';
 var tipbox = document.createElement("div");
 tipbox.id = "tipbox"
 tipbox.className = "mybox";
-var aFetch = document.createElement("a");
-aFetch.id = "fetch";
-aFetch.className = "btn";
-aFetch.href = "#";
-aFetch.innerText = "开始抽取";
-aFetch.addEventListener("click", function(){
+var devbox = document.createElement("div");
+devbox.id = "devbox"
+devbox.className = "mybox";
+var aDev = document.createElement("a");
+aDev.id = "dev";
+aDev.className = "btn";
+aDev.href = "#";
+aDev.innerText = "开发者工具";
+aDev.addEventListener("click", function(){
+    if (document.getElementById('devbox') == null)
+        document.body.appendChild(devbox);
+}, false);
+var aClose = document.createElement("a");
+aClose.id = "close";
+aClose.className = "btn";
+aClose.href = "#";
+aClose.innerText = "关闭";
+var aScan = document.createElement("a");
+aScan.id = "scan";
+aScan.className = "btn";
+aScan.href = "#";
+aScan.innerText = "抽取未提交";
+aScan.addEventListener("click", function(){
+    judScan = 1;
     getQuestionList();
+    if (document.getElementById('jsonbox') != null)
+        document.getElementById('jsonbox').remove();
     var jsonQuestionList = JSON.stringify(questionList);
     var jsonbox = document.createElement("div");
     jsonbox.id = "jsonbox"
@@ -124,6 +167,34 @@ aFetch.addEventListener("click", function(){
     pJson.className = "centerText";
     pJson.innerText = jsonQuestionList;
     jsonbox.appendChild(pJson);
+    jsonbox.appendChild(aClose.cloneNode(true));
+    jsonbox.children[1].addEventListener("click", function(){
+        document.getElementById("close").parentNode.remove();
+    }, false);
+    document.body.appendChild(jsonbox);
+}, false);
+var aFetch = document.createElement("a");
+aFetch.id = "fetch";
+aFetch.className = "btn";
+aFetch.href = "#";
+aFetch.innerText = "抽取已提交";
+aFetch.addEventListener("click", function(){
+    judScan = 0;
+    getQuestionList();
+    if (document.getElementById('jsonbox') != null)
+        document.getElementById('jsonbox').remove();
+    var jsonQuestionList = JSON.stringify(questionList);
+    var jsonbox = document.createElement("div");
+    jsonbox.id = "jsonbox"
+    jsonbox.className = "mybox";
+    var pJson = document.createElement("p");
+    pJson.className = "centerText";
+    pJson.innerText = jsonQuestionList;
+    jsonbox.appendChild(pJson);
+    jsonbox.appendChild(aClose.cloneNode(true));
+    jsonbox.children[1].addEventListener("click", function(){
+        document.getElementById("close").parentNode.remove();
+    }, false);
     document.body.appendChild(jsonbox);
 }, false);
 var aMatch = document.createElement("a");
@@ -150,6 +221,15 @@ aMatch.addEventListener("click", function(){
             break;
         case "逻辑和批判性思维（六）":
             targetJson = luoji1Json;
+            break;
+        case "创业者与创业团队（下）":
+            targetJson = chuangye1Json;
+            break;
+        case "创业初期的营销管理（三）":
+            targetJson = chuangye2Json;
+            break;
+        case "走访身边的创业者（三）":
+            targetJson = chuangye3Json;
             break;
         default:
             alert("目前题库尚未有此作业数据");
@@ -193,8 +273,21 @@ aMatch.addEventListener("click", function(){
 var pTotalNum = document.createElement("p");
 pTotalNum.className = "centerText";
 pTotalNum.innerText = "题目总数:" + imgList.length + " 答题成功数:0";
-tipbox.appendChild(aFetch);
+var pAuthor = document.createElement("p");
+pAuthor.className = "centerText";
+pAuthor.innerText = "凤凰院天真";
+pAuthor.addEventListener("click", function(){
+    pAuthor.innerText = "dwayneten.com";
+    setTimeout(function(){
+        pAuthor.innerText = "凤凰院天真";
+    }, 500);
+}, false);
+pAuthor.style.marginBottom = "5px";
+tipbox.appendChild(aDev);
 tipbox.appendChild(aMatch);
 tipbox.appendChild(pTotalNum);
+tipbox.appendChild(pAuthor);
+devbox.appendChild(aFetch);
+devbox.appendChild(aScan);
 loadStyleString(cssLayout);
 document.body.appendChild(tipbox);
